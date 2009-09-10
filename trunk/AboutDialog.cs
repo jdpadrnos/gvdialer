5d3f9e53b1c150f750adb4728f6bb776d12f4fb4
Chris.Ostler <Chris.Ostler@cdf865cc-9852-11de-a05d-3bd73e269465>
2009-09-10 04:46:43 +0000

    Add About dialog

---
+++ trunk/AboutDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace GVDialer
+{
+    public partial class AboutDialog : Form
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleOkClick(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void HandleLinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Control control = (Control)sender;
+            Process.Start(control.Text);
+        }
+    }
+}
