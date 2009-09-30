782af8c65c0c76d1674e9a6e87ea03bdc5a6496b
Chris.Ostler <Chris.Ostler@cdf865cc-9852-11de-a05d-3bd73e269465>
2009-09-30 06:20:19 +0000

    Add version to About dialog

--- AboutDialog.cs
+++ AboutDialog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace GVDialer
@@ -9,6 +10,20 @@ namespace GVDialer
         public AboutDialog()
         {
             InitializeComponent();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version;
+
+            mVersionLabel.Text += string.Format("{0}.{1}", version.Major, version.Minor);
+            if (version.Build > 0)
+            {
+                mVersionLabel.Text += string.Format(".{0}", version.Build);
+            }
+
+            if (version.Revision > 0)
+            {
+                mVersionLabel.Text += string.Format(" (revision {0})", version.Revision);
+            }
         }
 
         private void HandleOkClick(object sender, EventArgs e)
