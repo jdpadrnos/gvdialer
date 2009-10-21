d80d1a44a58a86c02ceb49eb2bf172468b873a70
Chris.Ostler <Chris.Ostler@cdf865cc-9852-11de-a05d-3bd73e269465>
2009-10-21 02:45:18 +0000

    Fix error dialog resizing

--- trunk/ErrorDialog.Designer.cs
+++ trunk/ErrorDialog.Designer.cs
@@ -38,27 +38,21 @@
             // 
             // mMessageLabel
             // 
-            this.mMessageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.mMessageLabel.AutoSize = true;
             this.mMessageLabel.Location = new System.Drawing.Point(13, 10);
+            this.mMessageLabel.MaximumSize = new System.Drawing.Size(400, 300);
             this.mMessageLabel.Name = "mMessageLabel";
-            this.mMessageLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
-            this.mMessageLabel.Size = new System.Drawing.Size(154, 23);
+            this.mMessageLabel.Size = new System.Drawing.Size(50, 13);
             this.mMessageLabel.TabIndex = 2;
             this.mMessageLabel.Text = "Message";
             // 
             // mDetailLink
             // 
-            this.mDetailLink.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.mDetailLink.AutoSize = true;
-            this.mDetailLink.Location = new System.Drawing.Point(13, 33);
+            this.mDetailLink.Location = new System.Drawing.Point(13, 23);
             this.mDetailLink.Name = "mDetailLink";
-            this.mDetailLink.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
-            this.mDetailLink.Size = new System.Drawing.Size(154, 23);
+            this.mDetailLink.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.mDetailLink.Size = new System.Drawing.Size(68, 23);
             this.mDetailLink.TabIndex = 1;
             this.mDetailLink.TabStop = true;
             this.mDetailLink.Text = "More detail...";
@@ -66,16 +60,12 @@
             // 
             // mDetailLabel
             // 
-            this.mDetailLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.mDetailLabel.AutoEllipsis = true;
             this.mDetailLabel.AutoSize = true;
-            this.mDetailLabel.Location = new System.Drawing.Point(13, 56);
-            this.mDetailLabel.MaximumSize = new System.Drawing.Size(500, 500);
+            this.mDetailLabel.Location = new System.Drawing.Point(13, 46);
+            this.mDetailLabel.MaximumSize = new System.Drawing.Size(600, 600);
             this.mDetailLabel.Name = "mDetailLabel";
-            this.mDetailLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
-            this.mDetailLabel.Size = new System.Drawing.Size(154, 23);
+            this.mDetailLabel.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.mDetailLabel.Size = new System.Drawing.Size(34, 23);
             this.mDetailLabel.TabIndex = 3;
             this.mDetailLabel.Text = "Detail";
             this.mDetailLabel.Visible = false;
@@ -84,7 +74,8 @@
             // 
             this.mOkButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.mOkButton.AutoSize = true;
-            this.mOkButton.Location = new System.Drawing.Point(52, 86);
+            this.mOkButton.Location = new System.Drawing.Point(13, 82);
+            this.mOkButton.Margin = new System.Windows.Forms.Padding(3, 13, 3, 3);
             this.mOkButton.Name = "mOkButton";
             this.mOkButton.Size = new System.Drawing.Size(75, 23);
             this.mOkButton.TabIndex = 0;
@@ -102,7 +93,6 @@
             this.mTableLayoutPanel.Controls.Add(this.mDetailLabel, 0, 2);
             this.mTableLayoutPanel.Controls.Add(this.mDetailLink, 0, 1);
             this.mTableLayoutPanel.Controls.Add(this.mMessageLabel, 0, 0);
-            this.mTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.mTableLayoutPanel.Name = "mTableLayoutPanel";
             this.mTableLayoutPanel.Padding = new System.Windows.Forms.Padding(10);
@@ -111,7 +101,7 @@
             this.mTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.mTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.mTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.mTableLayoutPanel.Size = new System.Drawing.Size(180, 122);
+            this.mTableLayoutPanel.Size = new System.Drawing.Size(101, 118);
             this.mTableLayoutPanel.TabIndex = 1;
             // 
             // ErrorDialog
@@ -119,14 +109,12 @@
             this.AcceptButton = this.mOkButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(180, 122);
             this.Controls.Add(this.mTableLayoutPanel);
             this.Icon = global::GVDialer.Properties.Resources.ActiveIcon;
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(500, 700);
             this.MinimizeBox = false;
             this.Name = "ErrorDialog";
             this.ShowInTaskbar = false;
